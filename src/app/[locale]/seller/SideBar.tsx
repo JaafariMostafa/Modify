2e5171e86fff35a9abc3dee3da2a2df59c355185
stoph2003 <mostafaimrane9@gmail.com>
2025-05-30 17:20:45 +0000

    add the arabic language for the sidebar

--- src/app/[locale]/seller/SideBar.tsx
+++ src/app/[locale]/seller/SideBar.tsx
@@ -1,15 +1,16 @@
 'use client';
 import { Box, ChartNoAxesCombined, Codesandbox, Home, MessageCircleQuestion, MessagesSquare, NotepadTextDashed, Settings } from "lucide-react";
+import { useLocale, useTranslations } from "next-intl";
 
 
 // ------ Generals
 const General_Links = [
     {
-        label: 'Dashboard',
+        label: 'sidebar.general_links.general_labels.0.label',
         href: '/seller',
         icon: Home,
     },{
-        label: 'Statistics',
+        label: 'sidebar.general_links.general_labels.1.label',
         href: '/statistics',
         icon: ChartNoAxesCombined,
     },
@@ -17,15 +18,15 @@ const General_Links = [
 // ------ Shop Section
 const Shop_Links = [
     {
-        label: 'Orders',
+        label: 'sidebar.shop_links.shop_labels.0.label',
         href: '/orders',
         icon: Box,
     },{
-        label: 'My Templates',
+        label: 'sidebar.shop_links.shop_labels.1.label',
         href: '/mytemplates',
         icon: NotepadTextDashed,
     },{
-        label: 'Notifications',
+        label: 'sidebar.shop_links.shop_labels.2.label',
         href: '/notifications',
         icon: MessagesSquare,
     }
@@ -33,17 +34,20 @@ const Shop_Links = [
 // ------ Support
 const Support_Links = [
     {
-        label: 'Settings',
+        label: 'sidebar.support_links.support_labels.0.label',
         href: '/settings',
         icon: Settings,
     },{
-        label: 'Help',
+        label: 'sidebar.support_links.support_labels.1.label',
         href: '/help',
         icon: MessageCircleQuestion,
     },
 ];
 export default function SideBar(){
 
+    const t = useTranslations('dashboard_seller_page');
+    const locale = useLocale();
+    const IsArabic = locale === 'ar';
     return (
         <main 
             className="w-64 flex-shrink-0 h-screen sticky top-0">
@@ -52,59 +56,71 @@ export default function SideBar(){
                 <span className="border border-[#892CDC] p-0.5 rounded"><Codesandbox size={22} /></span>Modify<span className="dark:text-white text-neutral-800">.ma</span>
             </h1>
             </div>
-            <nav className="p-4 space-y-2">
-                <h4 className="text-xs uppercase dark:text-neutral-600 text-neutral-400">General</h4>
-                <ul>
-                    {General_Links.map((link, index) => {
-                        return (
-                            <li 
-                            key={index} 
-                            className="px-4 py-2 rounded-lg dark:text-neutral-400 text-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800 
-                                    flex items-center gap-2 transition-colors">
-                                <link.icon size={25} /> {link.label}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-            <nav className="px-4 space-y-2">
-                <h4 className="text-xs uppercase dark:text-neutral-600 text-neutral-400">Shop</h4>
-                <ul>
-                    {Shop_Links.map((link, index) => {
-                        return (
-                            <li 
-                            key={index} 
-                            className="relative px-4 py-2 rounded-lg dark:text-neutral-400 text-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800 
-                                    flex items-center gap-2 transition-colors">
-                                <link.icon size={25} /> {link.label} {link.label.toLowerCase() === 'notifications' && (
-                                    <span 
+            <section dir={IsArabic ? 'rtl' : 'ltr'}>
+                <nav className="p-4 space-y-2">
+                    <h4 
+                        className="text-xs uppercase 
+                            dark:text-neutral-600 text-neutral-400">
+                                {t('sidebar.general_links.primary_title')}
+                    </h4>
+                    <ul>
+                        {General_Links.map((link, index) => {
+                            return (
+                                <li 
+                                key={index} 
+                                className="capitalize px-4 py-2 rounded-lg dark:text-neutral-400 text-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800 
+                                        flex items-center gap-2 transition-colors">
+                                    <link.icon size={25} /> {t(link.label)}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+                <nav className="px-4 space-y-2">
+                    <h4 className="text-xs uppercase dark:text-neutral-600 
+                    text-neutral-400">
+                        {t('sidebar.shop_links.primary_title')}
+                    </h4>
+                    <ul>
+                        {Shop_Links.map((link, index) => {
+                            return (
+                                <li 
+                                key={index} 
+                                className="relative capitalize px-4 py-2 rounded-lg dark:text-neutral-400 text-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800 
+                                flex items-center gap-2 transition-colors">
+                                    <link.icon size={25} /> {t(link.label)} {link.label.toLowerCase() === 'notifications' && (
+                                        <span 
                                         className="absolute right-2 text-sm px-2 py-0.5
-                                            rounded border dark:border-neutral-800 
-                                            border-[#892CDC] bg-[#892CDC]/30 
-                                            dark:bg-neutral-900/60 dark:text-neutral-400 text-[#892CDC]">
-                                                0
-                                    </span>
-                                )}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
-            <nav className="p-4 space-y-2">
-                <h4 className="text-xs uppercase dark:text-neutral-600 text-neutral-400">Support</h4>
-                <ul>
-                    {Support_Links.map((link, index) => {
-                        return (
-                            <li 
-                            key={index} 
-                            className="px-4 py-2 rounded-lg dark:text-neutral-400 text-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800 
-                                    flex items-center gap-2 transition-colors">
-                                <link.icon size={25} /> {link.label}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
+                                                rounded border dark:border-neutral-800 
+                                                border-[#892CDC] bg-[#892CDC]/30 
+                                                dark:bg-neutral-900/60 dark:text-neutral-400 text-[#892CDC]">
+                                                    0
+                                        </span>
+                                    )}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+                <nav className="p-4 space-y-2">
+                    <h4 className="text-xs uppercase dark:text-neutral-600 
+                    text-neutral-400">
+                        {t('sidebar.support_links.primary_title')}
+                    </h4>
+                    <ul>
+                        {Support_Links.map((link, index) => {
+                            return (
+                                <li 
+                                key={index} 
+                                className="capitalize px-4 py-2 rounded-lg dark:text-neutral-400 text-neutral-600 hover:bg-neutral-200 dark:hover:bg-neutral-800 
+                                        flex items-center gap-2 transition-colors">
+                                    <link.icon size={25} /> {t(link.label)}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </section>
         </main>
     )
 }
