46de71467192d22e1a02ea8754b3057f1ba07cf5
MostafaJaafari <professionaljaafari2003@gmail.com>
2025-06-05 12:53:38 +0000

    change some of hero styles

--- src/Components/CTAButton.tsx
+++ src/Components/CTAButton.tsx
@@ -12,9 +12,9 @@ export default async function CTAButton(){
     const session = await getSSSession();
     return (
         <Link href={session ? '/seller' : '/auth/login'}
-            className="flex bg-[#892CDC] hover:bg-[#892CDC]/90
+            className="flex justify-between bg-[#892CDC] hover:bg-[#892CDC]/90
                 transition-all duration-200 items-center gap-2 
-                py-2 px-6 rounded-lg text-white border border-[#892CDC]">
+                py-2 min-w-48 px-6 rounded-lg text-white border border-[#892CDC]">
             {t('hero_cta_button')} <ArrowUpRight className={` ${Rotate_Icon}`} size={20} />
         </Link>
     )
--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -10,10 +10,15 @@ export default function HeroSection() {
     const IsArabic = locale === 'ar';
     const Rotate_Icon = IsArabic ? '-rotate-90' : '';
     return (
-        <main className="w-full min-h-80 lg:px-20 md:px-12 px-6 pb-6 pt-12">
-            <section className="w-full flex flex-col lg:flex-row items-center justify-between space-y-8 lg:space-y-0">
+        <main className="w-full min-h-80 lg:px-20 md:px-12 px-6 pb-6 pt-20">
+            <section 
+                className="w-full flex flex-col
+                    items-center justify-center space-y-8 
+                    lg:space-y-0">
                 {/* Left: Text Content */}
-                <div className="flex-1 flex flex-col lg:items-start md:items-start items-center space-y-4">
+                <div 
+                    className="w-full flex-1 flex flex-col lg:items-center 
+                        md:items-start items-center space-y-4">
                     <span
                         className="flex items-center gap-2 px-4 py-1 border-[#892CDC] 
                             text-[#892CDC] rounded-full text-sm 
@@ -21,25 +26,29 @@ export default function HeroSection() {
                         <Star size={20} className="text-[#892CDC] fill-[#892CDC]" /> {t('printOnDemandPremium')}
                     </span>
                     <h1
-                        className={`lg:text-[10vh] md:text-[9vh] text-5xl flex flex-col primary-color font-bold
-                            py-4 lg:w-3/4 dark:text-white Fade-In lg:text-start md:text-start text-center
+                        className={`lg:text-[10vh] md:text-[9vh] text-5xl 
+                            flex flex-col primary-color font-bold
+                            py-4 lg:w-3/4 dark:text-white Fade-In 
+                            text-center
                             ${IsArabic ? "lg:gap-8 md:gap-8 sm:gap-6 gap-4" : ""}`}>
                         {t('hero_title')} <span className="text-[#892CDC]">{t('hero_title_marks')}</span>
                     </h1>
-                    <p className="text-xl lg:text-start md:text-start text-center dark:text-white text-neutral-600">
+                    <p 
+                        className="text-xl text-center dark:text-white 
+                            text-neutral-600">
                         {t('noStockNoCapitalNoExperience')} <br />
                         <span className="text-sm dark:text-neutral-400">
                             {t('modifyPartnerDescription')}
                         </span>
                     </p>
                     <div 
-                        className="py-6 flex flex-col w-full lg:flex-row md:flex-row 
-                            sm:flex-row gap-2 lg:items-start md:items-start items-center Fade-In">
+                        className="py-6 w-full flex justify-center 
+                            gap-2 items-center Fade-In">
                         <CTAButton />
                         <button
-                            className="flex bg-transparent hover:bg-[#892CDC]/10 dark:bg-neutral-900 dark:hover:bg-neutral-900/10 
+                            className="flex justify-between bg-transparent hover:bg-[#892CDC]/10 dark:bg-neutral-900 dark:hover:bg-neutral-900/10 
                                 transition-all duration-200 items-center gap-2 py-2 text-[#892CDC] dark:text-neutral-300
-                                px-6 rounded-lg border border-[#892CDC]/50 dark:border-neutral-800">
+                                min-w-48 px-6 rounded-lg border border-[#892CDC]/50 dark:border-neutral-800">
                             {t('hero_cta_button_secondary')} <ArrowUpRight className={` ${Rotate_Icon}`} size={20} />
                         </button>
                     </div>
